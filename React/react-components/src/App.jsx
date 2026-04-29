2cca1bc3f508bcfd59f061d7a5684d7601f9c934
Pathan-Irfan8007 <pathanirfan8007@gmail.com>
2026-04-29 17:39:14 +0530

    react-components

---
+++ React/react-components/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './assets/components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <h2>Hello</h2>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+      
+    </>
+  )
+}
+
+export default App
